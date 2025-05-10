a2128387255fd8fe44b9807f520dcfb86751adef
Zhoulai Fu <zhoulai.fu@gmail.com>
2025-05-10 18:27:36 +0900

    hw08.ml 120->100 total

--- homeworks/hw08.ml
+++ homeworks/hw08.ml
@@ -27,7 +27,7 @@ Use the following type declaration that represents arithmetic expressions.
       | Mul of exp * exp
       
 
-(* Exercise 1 (Points = 30)
+(* Exercise 1 (Points = 25)
 
 Encode the following arithmetic expressions as `exp`s:
 
@@ -44,13 +44,13 @@ let expression2 = failwith "Not Implemented"
 let expression3 = failwith "Not Implemented"
 
 
-(* Exercise 2 (Points = 30)
+(* Exercise 2 (Points = 25)
 Write the function eval: exp -> int, which reduces expressions to integer values:
 *)
 
 let rec eval (e:exp):int= failwith "Not Implemented"
 
-(* Exercise 3 (Points = 30)
+(* Exercise 3 (Points = 25)
 Write the function print : exp -> string , which returns a string representing `e`. The string should print arithmetic operators using infix notation and properly parenthesize expressions. Your solution may be similar to the following examples.
 
 print (Add (Int 10, Int 5)) produces "(10 + 5)"
@@ -63,7 +63,7 @@ let rec print (e:exp):string = failwith "Not Implemented"
 
 
 (* 
-Exercise 4 (Points = 30)
+Exercise 4 (Points = 25)
 Write the function is_sorted: int list -> bool to determine if the integers in an int lst are in sorted in ascending order.
 
 *)
